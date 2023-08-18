5840697a9d1c027e66ab1e72cc281894a42e23ff
Tanishq Kumar <tanishqkumar272@gmail.com>
2023-08-18 13:22:53 +0100

    h

--- CW/On_Demand_Usage_CW/OnDemand_24H.py
+++ CW/On_Demand_Usage_CW/OnDemand_24H.py
@@ -245,7 +245,7 @@ def main():
         )
 
     SLACK_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-    SLACK_CHANNEL = "#cluster-bot-testing-usage"
+    SLACK_CHANNEL = "#proj-cluster-usage-usage"
     slack_client = slack.WebClient(token=SLACK_TOKEN)
 
     # Get the metrics
